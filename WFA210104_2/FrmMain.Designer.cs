980a6e5b440711e807f9a980014f6a20bd8e7d35
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-01-13 12:35:49 +0100

    Add project files.

---
+++ WFA210104_2/FrmMain.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WFA210104_2
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvTurazok = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbKep = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurazok)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKep)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(33, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Természetjárók nyilvántartása";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(63, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(315, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Új személy felvátele";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dgvTurazok
+            // 
+            this.dgvTurazok.AllowUserToAddRows = false;
+            this.dgvTurazok.AllowUserToDeleteRows = false;
+            this.dgvTurazok.AllowUserToResizeColumns = false;
+            this.dgvTurazok.AllowUserToResizeRows = false;
+            this.dgvTurazok.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTurazok.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvTurazok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTurazok.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvTurazok.Location = new System.Drawing.Point(12, 255);
+            this.dgvTurazok.Name = "dgvTurazok";
+            this.dgvTurazok.RowHeadersVisible = false;
+            this.dgvTurazok.RowHeadersWidth = 51;
+            this.dgvTurazok.RowTemplate.Height = 24;
+            this.dgvTurazok.Size = new System.Drawing.Size(823, 158);
+            this.dgvTurazok.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Azonosító";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Vezetéknév";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Keresztnév";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Város";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Telefonszám";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            // 
+            // pbKep
+            // 
+            this.pbKep.Image = global::WFA210104_2.Properties.Resources.kep;
+            this.pbKep.Location = new System.Drawing.Point(609, 12);
+            this.pbKep.Name = "pbKep";
+            this.pbKep.Size = new System.Drawing.Size(226, 223);
+            this.pbKep.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbKep.TabIndex = 3;
+            this.pbKep.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(34, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(534, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "A nyilvántartásban szereplő túrázók adatai:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 429);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(823, 68);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(444, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(315, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Adatok frissítése";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 518);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pbKep);
+            this.Controls.Add(this.dgvTurazok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Túra";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurazok)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKep)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvTurazok;
+        private System.Windows.Forms.PictureBox pbKep;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WFA210104_2/FrmMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA210104_2
+{
+    public partial class FrmMain : Form
+    {
+        SqlConnection conn;
+
+        public FrmMain()
+        {
+            //conn = new SqlConnection(
+            //    @"Data Source = (localdb)\MSSQLLocalDB; " +
+            //    "Initial Catalog = tura;");
+
+            conn = new SqlConnection(
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                @"AttachDbFilename=|DataDirectory|Resources\tura.mdf");
+
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var r = new SqlCommand("SELECT * FROM turazo", conn)
+                .ExecuteReader();
+
+            while (r.Read())
+            {
+                dgvTurazok.Rows.Add(r[0], r[1], r[2], r[3], r[4]);
+            }
+            conn.Close();
+
+        }
+    }
+}
